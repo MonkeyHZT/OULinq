578dce1c2e5e71c00da8b7c0dcc8601b4eb6ce22
MonkeyHZT <houzhitong@qq.com>
2015-11-16 20:29:21 +0800

    创建了项目

---
+++ OULinqDemo/OULinqDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OULinqDemo
+//
+//  Created by HouZhiTong on 15/11/16.
+//  Copyright © 2015年 com.houzhitong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OULinqDemo/OULinqDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OULinqDemo
+//
+//  Created by HouZhiTong on 15/11/16.
+//  Copyright © 2015年 com.houzhitong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
