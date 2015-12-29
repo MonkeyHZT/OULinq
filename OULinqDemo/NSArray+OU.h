56aad950e8cad4cb19b46b42e2243842f17fa4fc
houzhitong <houzhitong@qq.com>
2015-12-29 14:54:33 +0800

    添加了NSArray的First,FirstOrNull,Where函数

--- OULinqDemo/NSArray+OU.h
+++ OULinqDemo/NSArray+OU.h
@@ -42,5 +42,35 @@ typedef BOOL (^BoolBlock)(id parameter);
  *  @return YES表示至少有一个满足条件.NO表示全部不满足
  */
 -(BOOL)Any:(BoolBlock)boolBlock;
+/**
+ *  @author Hzt, 2016-12-29 14:12:05
+ *
+ *  @brief 返回第一个满足条件的对象.如果没有任何满足的会抛出异常
+ *
+ *  @param boolBlock block表达式.表示是否满足条件
+ *
+ *  @return 第一个满足条件的对象.没有则抛异常
+ */
+- (id)First:(BoolBlock)boolBlock;
+/**
+ *  @author Hzt, 2016-12-29 14:12:12
+ *
+ *  @brief 返回第一个满足条件的对象.如果没有找到则返回Null
+ *
+ *  @param boolBlock block表达式.表示是否满足条件
+ *
+ *  @return 第一个满足条件的对象.没有则返回nil
+ */
+- (id)FirstOrNull:(BoolBlock)boolBlock;
+/**
+ *  @author Hzt, 2016-12-29 14:12:12
+ *
+ *  @brief 返回所有满足表达式条件的对象
+ *
+ *  @param boolBlock block表达式.表示是否满足条件
+ *
+ *  @return 所有满足条件的对象集合.如果没有找到则count为0
+ */
+-(NSArray *)Where:(BoolBlock)boolBlock;
 
 @end
--- OULinqDemo/NSArray+OU.m
+++ OULinqDemo/NSArray+OU.m
@@ -10,28 +10,71 @@
 
 @implementation NSArray (OU)
 
-- (BOOL)All:(BoolBlock)boolBlock
-{
-    for (int i = 0; i < [self count]; i++) {
-        if (!boolBlock(self[i])) {
-            return NO;
-        }
+- (BOOL)All:(BoolBlock)boolBlock {
+  for (int i = 0; i < [self count]; i++) {
+    if (!boolBlock(self[i])) {
+      return NO;
     }
-    return YES;
+  }
+  return YES;
 }
 
-- (BOOL)Any
-{
-    return [self count] > 0;
-} 
+- (BOOL)Any {
+  return [self count] > 0;
+}
 
-- (BOOL)Any:(BoolBlock)boolBlock
-{
-    for (int i = 0; i < [self count]; i++) {
-        if (boolBlock(self[i])) {
-            return YES;
-        }
+- (BOOL)Any:(BoolBlock)boolBlock {
+  for (int i = 0; i < [self count]; i++) {
+    if (boolBlock(self[i])) {
+      return YES;
     }
-    return NO;
+  }
+  return NO;
+}
+
+- (id)First:(BoolBlock)boolBlock {
+  id obj = [self FirstOrNull:boolBlock];
+  if (!obj) {
+    [NSException raise:@"没有找到任何满足条件的对象"
+                format:@"没有找到任何满足条件的对象"];
+  }
+  return obj;
+}
+/**
+ *  @author Hzt, 2016-12-29 14:12:12
+ *
+ *  @brief 返回第一个满足条件的对象.如果没有找到则返回Null
+ *
+ *  @param boolBlock block表达式.表示是否满足条件
+ *
+ *  @return 第一个满足条件的对象.没有则返回nil
+ */
+- (id)FirstOrNull:(BoolBlock)boolBlock {
+  for (int i = 0; i < [self count]; i++) {
+    id item = [self objectAtIndex:i];
+    if (boolBlock(item)) {
+      return item;
+    }
+  }
+  return nil;
+}
+/**
+ *  @author Hzt, 2016-12-29 14:12:12
+ *
+ *  @brief 返回所有满足表达式条件的对象
+ *
+ *  @param boolBlock block表达式.表示是否满足条件
+ *
+ *  @return 所有满足条件的对象集合.如果没有找到则count为0
+ */
+- (NSArray *)Where:(BoolBlock)boolBlock {
+  NSMutableArray *result = [NSMutableArray array];
+  for (int i = 0; i < [self count]; i++) {
+    id item = [self objectAtIndex:i];
+    if (boolBlock(item)) {
+      [result addObject:item];
+    }
+  }
+  return result.copy;
 }
 @end
